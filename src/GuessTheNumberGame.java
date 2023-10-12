ba7c9dabd94dd655b85a082c4c93994b79b096a7
beatrizsuyo <beatrizsuyo@gmail.com>
2023-10-12 12:33:52 -0500

    implementando test

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -6,36 +6,38 @@ import java.util.Scanner;
 
 // Clase abstracta para representar a un jugador genérico
  abstract class Player {
+    //private. solo los metodos dentro de Player pueden modificar
     private String name;
+    //protected solo las sub-clases pueden acceder
     protected List<Integer> guesses;
-
+//constructor: iniciliza NAME y crea lista GUESSES
     public Player(String name) {
         this.name = name;
         this.guesses = new ArrayList<>();
     }
-    
+    //metodo abs se implementa en las sub-clases
     public abstract int makeGuess();
-
+     //metodo obtener nombre
     public String getName() {
         return name;
     }
-
+    //metodo para obtener suposiciones
     public List<Integer> getGuesses() {
         return guesses;
+    }
+
+    public Object getTargetNumber() {
+        return null;
     }
 }
 
 // Clase para representar al jugador humano
 class HumanPlayer extends Player {
-    Scanner scanner;
+    Scanner scanner;//lee entrada del usuario
 
     public HumanPlayer(String name) {
+        //trae de Player atributos y metodos
         super(name);
-        scanner = new Scanner(System.in);
-    }
-
-    public HumanPlayer(String string, SimulatedUserInput userInput) {
-        super(string);
         scanner = new Scanner(System.in);
     }
 
@@ -46,10 +48,11 @@ class HumanPlayer extends Player {
         scanner.nextLine(); // la nueva línea después del número
         return guess;
     }
-
+/* 
     public void setScanner(Scanner scanner2) {
         scanner = scanner2;
     }
+    */
 }
 
 // Clase para representar al jugador computadora
@@ -60,7 +63,7 @@ class ComputerPlayer extends Player {
     public ComputerPlayer(String name, int targetNumber) {
         super(name);
         this.targetNumber = targetNumber;
-        // random
+    
         random = new Random();
     }
     
@@ -93,11 +96,14 @@ class ComputerPlayer extends Player {
         return guess;
     }
 
+    public Object getTargetNumber() {
+        return null;
+    }
+
     public void setRandom(Random random2) {
     }
 
-    public Object getTargetNumber() {
-        return null;
+    public void setTargetNumber(int targetNumber2) {
     }
         
 }
@@ -123,8 +129,8 @@ public class GuessTheNumberGame {
             currentPlayer.guesses.add(guess);
 
             if (guess == targetNumber) {
-                System.out.println("¡Felicidades!");
-                System.out.println("¡" + currentPlayer.getName() + " ha adivinado el número " + targetNumber + "!");
+                System.out.println(" ¡Felicidades! " );
+                System.out.println("¡" + currentPlayer.getName() + " ha adivinado el número  " + targetNumber + "!");
                 System.out.println("Historial de suposiciones de " + currentPlayer.getName() + ": " + currentPlayer.getGuesses());
                 break; 
                 
--- src/GuessTheNumberGameTest.java
+++ src/GuessTheNumberGameTest.java
@@ -1,13 +1,69 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Random;
+//import org.mockito.Mockito;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+
+
+public class GuessTheNumberGameTest {
+    private ComputerPlayer computerPlayer;
+
+    @Before
+    public void setUp() {
+        new HumanPlayer("Jugador Humano");
+
+        // Configura el objeto ComputerPlayer con un número objetivo fijo
+        computerPlayer = new ComputerPlayer("Jugador Computadora", 44);
+
+        // Esto es para que el jugador de la computadora no genere números aleatorios, sino que siga la estrategia.
+        computerPlayer.setRandom(new Random(44));
+    }
+
+    @Test
+    public void testComputerPlayerMakeGuess() {
+        // Verificar que el jugador de computadora haga una suposición válida
+        int guess = computerPlayer.makeGuess();
+        assertTrue(guess >= 1 && guess <= 100);
+        // se puede hacer más suposiciones y verificar si se acercan al objetivo.
+    }
+
+    @Test
+    public void testPlayerUpdatesGuessesAfterGuess() {
+        // Crea un objeto mock de la clase Player
+        Player player = new ComputerPlayer("Mocked Computer Player", 50);
+
+        // la lista de suposiciones esté vacía al principio
+        assertTrue(player.getGuesses().isEmpty());
+
+        // Hacer una suposición
+        player.makeGuess();
+
+        // Verificar que la lista de suposiciones no esté vacía después de hacer una suposición
+        assertEquals(1, player.getGuesses().size());
+    }
+
+}
+
+
+
+
+
+
+
+/**********************************************************************************************************************************
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+//import static org.mockito.Mockito.when;
+//import static org.mockito.Mockito.when;
+
+//import java.util.ArrayList;
+//import java.util.List;
+import java.util.Random;
+import org.junit.Before;
+import org.junit.Test;
+//import org.mockito.Mockito;
 
 public class GuessTheNumberGameTest {
     private HumanPlayer humanPlayer;
@@ -21,22 +77,41 @@ public class GuessTheNumberGameTest {
 
         // Configura el objeto ComputerPlayer con un número objetivo fijo
         computerPlayer = new ComputerPlayer("Jugador Computadora", 44);
+        //esto es para que el jugador computadora no genere aleatorios,sino siga la estrategia
+        computerPlayer.setRandom(new Random(44));
     }
-
-    @Test
-    public void testHumanPlayerMakeGuess() {
-        // Verificar que el jugador humano ingrese la suposición correcta
-        int guess = humanPlayer.makeGuess();
-        assertEquals(44, guess);
-    }
-
+    
+    
     @Test
     public void testComputerPlayerMakeGuess() {
         // Verificar que el jugador de computadora haga una suposición válida
         int guess = computerPlayer.makeGuess();
         assertTrue(guess >= 1 && guess <= 100);
+        //para probar la logica de la soposicion de la computadora
+        //se puede hacer mas suposiciones y verificar si se acercan al numero objetivo
     }
+    @Test
+    public void testPlayerUpdatesGuessesAfterGuess(){
+         // Crear un objeto mock de la clase Player
+         Player player = new ComputerPlayer("Mocked Computer Player", 50);
+
+         // Asegúrate de que la lista de suposiciones esté vacía al principio
+         assertTrue(player.getGuesses().isEmpty());
+ 
+         // Hacer una suposición
+         player.makeGuess();
+ 
+         // Verificar que la lista de suposiciones no esté vacía después de hacer una suposición
+         assertEquals(1, player.getGuesses().size());
+     }
     
+}
+
+
+
+*/
+//****************************************************************************************************************** */
+    /*
     @Test
     public void testPlayerGuessesAfterWinning() {
         // Crear un objeto mock de la clase Player
@@ -54,9 +129,9 @@ public class GuessTheNumberGameTest {
         // Verificar el comportamiento del jugador después de ganar
         assertEquals(simulatedGuesses, player.getGuesses());
     }
-}
 
-/* 
+     */
+     /* 
     @Test
     public void testCo
 
@@ -68,7 +143,7 @@ public class GuessTheNumberGameTest {
             assertTrue(guess >= 1 && guess <= 100);
         }
     }
-}
-*/
+
+    */
 
 
